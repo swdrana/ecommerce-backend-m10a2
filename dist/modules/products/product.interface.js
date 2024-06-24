9410cc77ca374495a4f15bb01c6b2c1deba1632b
swdrana <tctr2s@gmail.com>
2024-06-25 00:29:04 +0600

    Remove old files for new project

--- dist/modules/products/product.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// product.interface.ts
-Object.defineProperty(exports, "__esModule", { value: true });
--- src/modules/orders/order.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { orderController } from "./order.controller";
-const router = express.Router();
-
-router.post("/", orderController.createOrder);
-router.get("/", orderController.searchOrGetAllOrders);
-
-export const orderRoute = router
