59ab44415b2db77dc4e1857520b11e8afd15eb95
swdrana <tctr2s@gmail.com>
2024-06-27 16:20:25 +0600

    update Facility if that facility is deleted then send that error

--- src/modules/facility/facility.service.ts
+++ src/modules/facility/facility.service.ts
@@ -1,3 +1,5 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { TFacility } from "./facility.interface";
 import FacilityModel from "./facility.model";
 
@@ -5,16 +7,32 @@ const createFacilityIntoDb = async (payload: TFacility) => {
   return await FacilityModel.create(payload);
 };
 
-const updateFacilityUsingIdIntoDb = async (id:string,payload:Record<string,any>) =>{
-  return await FacilityModel.findByIdAndUpdate(id, payload, {new: true});
-}
+const updateFacilityUsingIdIntoDb = async (
+  id: string,
+  payload: Record<string, any>
+) => {
+  // if soft delete then dont send data
+  const isDeleted = await FacilityModel.findById(id);
+  console.log(isDeleted)
+  if (isDeleted?.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "This Facility is deleted");
+  }
+  const updatedFacility = await FacilityModel.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return updatedFacility;
+};
 
-const deleteFacilityByIDFromDb = async (id:string) =>{
-  return await FacilityModel.findByIdAndUpdate(id, {isDeleted:true}, {new: true});
-}
+const deleteFacilityByIDFromDb = async (id: string) => {
+  return await FacilityModel.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true }
+  );
+};
 
 export const facilityService = {
   createFacilityIntoDb,
   updateFacilityUsingIdIntoDb,
-  deleteFacilityByIDFromDb
-}
+  deleteFacilityByIDFromDb,
+};
